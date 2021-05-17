f1cc8b582afa0ebab8277a23edd20d4e57208e3b
TaeWon01 <77069235+TaeWon01@users.noreply.github.com>
2021-05-17 12:50:26 +1000

    Update bookingResult.js

--- js/bookingResult.js
+++ js/bookingResult.js
@@ -7,4 +7,4 @@ function loadBookingData(){
   const finalBookingResult = JSON.parse(localStorage.getItem("booking-data"));
   $("#complete-name").html(finalBookingResult["first_name"] + " " + finalBookingResult["last_name"]);
   $("#booking-dates").html(finalBookingResult["start_date"] + " - " + finalBookingResult["end_date"]);
-}
+  
